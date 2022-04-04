394554cd5223f970eb18bcbe2bcd221e9e313917
Hurairah Raza <99223946+hero88yay@users.noreply.github.com>
2022-04-04 19:55:32 -0400

    Add files via upload

---
+++ homew.js/hw.js
@@ -0,0 +1,51 @@
+//==========Exercise #1 ===========//
+/*
+Write a function that parses through the below object and displays all of their
+favorite food dishes as shown:
+*/
+
+let person3 = {
+    pizza:["Deep Dish","South Side Thin Crust"],
+    tacos:"Anything not from Taco bell",
+    burgers:"Portillos Burgers",
+    ice_cream:["Chocolate","Vanilla","Oreo"],
+    shakes:[{
+        oberwise:"Chocolate",
+        dunkin:"Vanilla",
+        culvers:"All of them",
+        mcDonalds:"Sham-rock-shake",
+        cupids_candies:"Chocolate Malt"
+    }]
+}
+
+for (let i=0;i < Object.keys(person3.length; i++)){
+    console.log('this persons fav food is'(object.keys(person3)[i]))
+      console.log(person3[object.keys])}
+
+    //i cant quite figure out how to start off excersice 2
+
+// =============Exercise #3 ============//
+/*
+
+    Create a Promised based function that will check a string to determine if it's length is greater than 10.
+    If the length is greater than ten console log "Big word". 
+    If the length of the string is less than 10 console log "Small Number"
+*/
+
+let promiseFunction=(number_given)=>{
+    if (number_given.length>10 ){
+        console.log(Big_word)
+        else{
+            console.log=false
+        }
+        const promiseFunction=(number_given)=>{
+            if (number_given.length<10 ){
+                console.log(Small_number)
+                else{
+                    console.log=false
+                }
+        
+
+    
+
+            
